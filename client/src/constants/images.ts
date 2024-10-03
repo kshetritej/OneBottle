d3cac5aea735b95ba8ae612edcc0e59af803a9da
kshetritej <tejghartikshetri@gmail.com>
2024-10-03 20:57:11 +0545

    update: images path

--- client/src/constants/images.ts
+++ client/src/constants/images.ts
@@ -1,9 +1,9 @@
-const tuborg_beer = "../../public/assets/tuborg_beer.jpg";
-const absolute_vodka = "../../public/assets/vodka.jpg";
-const unknown_wine = "../../public/assets/wine.jpg";
-const xamaka_wine = "../../public/assets/xamaka_wine.jpg";
-const carousel1 = "../../public/assets/carousel_1.jpg";
-const carousel2 = "../../public/assets/carousel_2.jpg";
-const carousel3 = "../../public/assets/carousel_3.jpg";
+const tuborg_beer = "/assets/tuborg_beer.jpg";
+const absolute_vodka = "/assets/vodka.jpg";
+const unknown_wine = "/assets/wine.jpg";
+const xamaka_wine = "/assets/xamaka_wine.jpg";
+const carousel1 = "/assets/carousel_1.jpg";
+const carousel2 = "/assets/carousel_2.jpg";
+const carousel3 = "/assets/carousel_3.jpg";
 
 export { tuborg_beer, absolute_vodka, unknown_wine, xamaka_wine, carousel1, carousel2, carousel3 }
