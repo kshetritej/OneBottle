dd39575d0c9f33d535d278d74349b02bfe7b23f2
kshetritej <tejghartikshetri@gmail.com>
2024-10-06 07:20:39 +0545

    add: category model

---
+++ server/Models/Category.cs
@@ -0,0 +1,9 @@
+namespace OneBottle.Models
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
