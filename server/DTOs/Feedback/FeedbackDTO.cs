55498b89fb6e9335cd4fb22e886b33089f3eb586
kshetritej <tejghartikshetri@gmail.com>
2024-10-16 10:18:38 +0545

    patch: feedback dto

--- server/DTOs/Feedback/FeedbackDTO.cs
+++ server/DTOs/Feedback/FeedbackDTO.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OneBottle.DTOs.Feedback
 {
     public class FeedbackDTO
     {
+        [Required]
         public Guid FeedbackId { get; set; }
+        [Required]
         public Guid UserId { get; set; }
+        [Required]
         public Guid ProductId { get; set; }
+        [Required, Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
         public int Rating { get; set; }
+        [Required,MaxLength(150, ErrorMessage = "Comment must be less than 150 characters")]
         public string Comment { get; set; } = string.Empty;
         public DateTime Date { get; set; }
     }
--- server/DTOs/Feedback/UpdateFeedbackDTO.cs
+++ server/DTOs/Feedback/UpdateFeedbackDTO.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OneBottle.DTOs.Feedback
 {
     public class UpdateFeedbackDTO
     {
+        [Required]
         public Guid FeedbackId { get; set; }
+        [Required]
         public Guid UserId { get; set; }
+        [Required]
         public Guid ProductId { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
         public int? Rating { get; set; }
+        [MaxLength(150, ErrorMessage = "Comment must be less than 150 characters")]
         public string? Comment { get; set; } = string.Empty;
         public DateTime Date { get; set; }
     }
